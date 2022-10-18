5480a1183c47dbe7d298d144e72b52d26fc9c80e
Raihan <17radf@gmail.com>
2022-10-18 22:08:49 +0700

    feat(querybuilder): add function queries and try catch's

--- core/database/querybuilder.php
+++ core/database/querybuilder.php
@@ -4,20 +4,57 @@ class QueryBuilder
 {
 
   protected $pdo;
+  private $errMsg = "error ngab(!!!): ";
 
   public function __construct(PDO $pdo)
   {
     $this->pdo = $pdo;
   }
 
-  public function selectAll($table)
+  public function login($username, $password)
   {
     // prepare statement and execute
-    $statement = $this->pdo->prepare("select * from ($table)");
-    $statement->execute();
+    try {
+      $statement = $this->pdo->prepare("select username, password from users
+      where username = '$username'
+      and password = '$password'");
+      $statement->execute();
+    } catch (PDOException $ex) {
+      return $this->errMsg . $ex->getMessage();
+    }
 
-    // store results to variable $result
     return $statement->fetchAll(PDO::FETCH_CLASS);
   }
 
+  public function selectAll($table)
+  {
+    // prepare statement and execute
+    try {
+      $statement = $this->pdo->prepare("select * from ($table)");
+      $statement->execute();
+    } catch (PDOException $ex) {
+      return $this->errMsg . $ex->getMessage();
+    }
+
+    return $statement->fetchAll(PDO::FETCH_CLASS);
+  }
+
+  public function insert($table, $param)
+  {
+    $sql = sprintf(
+      'insert into %s (%s) values (%s)',
+      $table,
+      implode(',', array_keys($param)),
+      ':' . implode(', :', array_keys($param))
+    );
+
+    try {
+      $statement = $this->pdo->prepare($sql);
+      $statement->execute($param);
+    } catch (PDOException $ex) {
+      return $this->errMsg . $ex->getMessage();
+    }
+
+    return "success";
+  }
 }
