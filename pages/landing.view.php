5a6dd4dc00db1d5e0eeec0de2273ac926d3aea49
Daffa Afifi Syahrony <afifidaffa16@gmail.com>
2022-10-21 17:28:28 +0700

    landing page: add navbar-header

---
+++ pages/landing.view.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/style.css">
+  <script src="https://cdn.tailwindcss.com" defer></script>
+  <title>Suki Store</title>
+</head>
+<body>
+    <header>
+        <div class="header-left">
+            <img class="logo" src="../assets/img/logo.png">
+        </div>
+        <nav class="header-right">
+            <a href="#beranda">Beranda</a>
+            <a href="#fitur">Fitur</a>
+            <a href="#kontak">Kontak</a>
+            <a href="login.view.php" class="btn-login shadow-lg shadow-gray-500/90">Masuk</a>
+        </nav>
+    </header>
+</body>
+</html>
