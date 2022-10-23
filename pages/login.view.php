03cd80a04d50b9b4c5ef528eda66a2b2fe3d9130
Daffa Afifi Syahrony <afifidaffa16@gmail.com>
2022-10-23 21:10:47 +0700

    add login page

--- pages/login.view.php
+++ pages/login.view.php
@@ -1,11 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../styles/style.css">
+  <script src="https://cdn.tailwindcss.com" defer></script>
+  <title>Suki Store</title>
+</head>
 <body>
-
-
-  <section class="main">
-    <h3>anggap ini login ok?</h3>
-    <a href="core/process/login.php" class="link">
-      <button class="btn btn-default">Okay</button>
-    </a>
+  <section class="shadow-2xl">
+    <div class="leftBox">
+      <h1>Suki Mebel</h1>
+    </div>
+    <div class="rightBox">
+      <div class="formBox">
+        <h2>Login</h2>
+        <form action="">
+          <div class="inputBox">
+            <span>Username</span>
+            <input type="text" name="username">
+          </div>
+          <div class="inputBox">
+            <span>Password</span>
+            <input type="text" name="password">
+          </div>
+          <div class="remember">
+            <label for="remember">
+              <input type="checkbox" name="remember"> Remember me
+            </label>
+          </div>
+          <div class="inputBox">
+            <input type="submit" name="login" value="Sign In">
+          </div>
+        </form>
+      </div>
+    </div>
   </section>
-
 </body>
+</html>
