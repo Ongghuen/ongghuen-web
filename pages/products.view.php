2b0e5e8202700aa556986ac1f73452bc873603c5
Daffa Afifi Syahrony <afifidaffa16@gmail.com>
2022-11-09 21:15:55 +0700

    modify navbar

--- pages/products.view.php
+++ pages/products.view.php
@@ -56,7 +56,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="./products.view.php">
+          <a class="nav-link" href="./orders.view.php">
             <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-cart text-warning text-sm opacity-10"></i>
             </div>
